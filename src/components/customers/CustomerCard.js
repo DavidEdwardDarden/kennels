86d2f10f5b5ecdbb4c5d631aded73f9688642a1d
David Darden <davidedarden83@gmail.com>
2021-04-02 10:12:54 -0500

    initial commit

---
+++ src/components/customers/CustomerCard.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Animal.css"
+
+export const CustomerCard = () => (
+    <section className="customer">
+        <h3 className="customer__name">James McDuck</h3>
+      
+    </section>
+)
