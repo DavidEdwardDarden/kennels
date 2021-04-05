55723b50f741f2bd12373d3a1090e50821d888f6
David Darden <davidedarden83@gmail.com>
2021-04-05 15:32:51 -0500

    added code in mutliple parts

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -3,7 +3,7 @@ import "./Animal.css"
 
 export const AnimalCard = () => (
     <section className="animal">
-        <h3 className="animal__name">Doodles</h3>
+        <h3 className="animal__name">{animal.name}</h3>
         <div className="animal__breed">Breed: Poodle</div>
     </section>
 )
