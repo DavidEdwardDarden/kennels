bbee7c32d6d7ddaff1cf212eaecfa0f946c31251
David Darden <davidedarden83@gmail.com>
2021-04-07 13:55:55 -0500

    got all the tabs to function

--- src/components/employees/EmployeeCard.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import "./Animal.css"
-
-export const EmployeeCard = () => (
-    <section className="animal">
-        <h3 className="animal__name">Roberto Gonzalez Phillipo Montana Jr the 2nd</h3>
-        <div className="animal__breed">Section: Sales Clerk</div>
-    </section>
-)
--- src/components/locations/LocationCard.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import "./Animal.css"
-
-export const LocationCard = () => (
-    <section className="location">
-        <h3 className="location__name">Las Vegas</h3>
-        <div className="location_streetaddress">Main St.</div>
-    </section>
-)
