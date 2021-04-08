4569a36b9bb5201b0dedc2637b264d887f1c527a
David Darden <davidedarden83@gmail.com>
2021-04-08 14:04:28 -0500

    login and edit

--- src/components/animal/AnimalCard.js
+++ src/components/animal/AnimalCard.js
@@ -1,15 +1,20 @@
 import React from "react"
 import "./Animal.css"
-import { Link } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
 
-export const AnimalCard = ({animal, handleDeleteAnimal}) => (
+// const history = useHistory();
+
+export const AnimalCard = ({ animal, handleDeleteAnimal }) => (
     <section className="animal">
         <img src={require('./dog.svg').default}></img>
         <h3 className="animal__name">{animal.name}</h3>
         <div className="animal__breed">Breed: {animal.breed}</div>
         <button type="button" onClick={() => handleDeleteAnimal(animal.id)}>Discharge</button>
+        <Link to={`/animals/${animal.id}/edit`}>
+            <button>Edit</button>
+        </Link>
         <Link to={`/animals/${animal.id}`}>
-  <button>Details</button>
-</Link>
+            <button>Details</button>
+        </Link>
     </section>
 )
